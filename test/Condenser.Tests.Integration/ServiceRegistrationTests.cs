878e700c7d06e6bab29f897fee3653f228bb19c0
Drawaes <seawardtim@gmail.com>
2016-10-23 15:09:28 +0100

    Added the first integration test

---
+++ test/Condenser.Tests.Integration/ServiceRegistrationTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Condenser.Tests.Integration
+{
+    public class ServiceRegistrationTests
+    {
+        [Fact]
+        public async Task TestSingleRegisterAndRetrieve()
+        {
+            var regClient = new CondenserDotNet.Client.ServiceRegistrationClient();
+            regClient.Config(serviceName : "TestService", serviceId : Guid.NewGuid().ToString(), address : "localhost", port : 7777);
+            regClient.AddUrls("api/testurl");
+            await regClient.RegisterServiceAsync();
+
+            //Now try to get it back
+            var endpointClient = new CondenserDotNet.Client.ServiceEndpointClient();
+            var serverAddress = await endpointClient.GetServiceAddressAsync("TestService");
+
+            Assert.Equal("localhost",serverAddress.Item1);
+            Assert.Equal(7777, serverAddress.Item2);
+        }
+    }
+}
