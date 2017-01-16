4f6a9910c51ff3de4faf647cd8c84e1c5905ffcd
Shapers <dean.shackley@gmail.com>
2017-01-16 22:36:12 +0000

    Refactor to extract blocking watcher to reuse for service watcher

---
+++ src/CondenserDotNet.Service/BlockingWatcher.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace CondenserDotNet.Service
+{
+    internal class BlockingWatcher<T>
+        where T : class
+    {
+        
+        private readonly AsyncManualResetEvent<bool> _haveFirstResults = new AsyncManualResetEvent<bool>();
+
+        private readonly HttpClient _client;
+        private readonly CancellationToken _cancel;
+        private readonly string _lookupUrl;
+        private T _serviceInstances;
+        private WatcherState _state = WatcherState.NotInitialized;
+
+        public BlockingWatcher(string url,
+            HttpClient client, CancellationToken cancel)
+        {
+            _client = client;
+            _cancel = cancel;
+            _lookupUrl = $"{url}?passing&index=";
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+            WatchLoop();
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+        }
+
+        internal async Task<T> SafeReadAsync()
+        {
+            if (!await _haveFirstResults.WaitAsync())
+            {
+                return null;
+            }
+            T instances = Volatile.Read(ref _serviceInstances);
+            return instances;
+        }
+
+        internal async Task WatchLoop()
+        {
+            try
+            {
+                string consulIndex = "0";
+                while (true)
+                {
+                    var result = await _client.GetAsync(_lookupUrl + consulIndex,
+                        _cancel);
+                    if (!result.IsSuccessStatusCode)
+                    {
+                        if (_state == WatcherState.UsingLiveValues)
+                        {
+                            _state = WatcherState.UsingCachedValues;
+                        }
+                        await Task.Delay(1000);
+                        continue;
+                    }
+                    consulIndex = result.GetConsulIndex();
+                    var content = await result.Content.ReadAsStringAsync();
+                    var listOfServices = JsonConvert.DeserializeObject<T>(content);
+                    Interlocked.Exchange(ref _serviceInstances, listOfServices);
+                    _state = WatcherState.UsingLiveValues;
+                    _haveFirstResults.Set(true);
+                }
+            }
+            catch (TaskCanceledException) { /*nom nom */}
+            catch (ObjectDisposedException) { /*nom nom */}
+        }
+    }
+}
--- src/CondenserDotNet.Service/ServiceWatcher.cs
+++ src/CondenserDotNet.Service/ServiceWatcher.cs
@@ -3,86 +3,35 @@ using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using CondenserDotNet.Service.DataContracts;
-using Newtonsoft.Json;
 
 namespace CondenserDotNet.Service
 {
     internal class ServiceWatcher
     {
         //used to randomly select an instance
-        static int seed = Environment.TickCount;
-        static readonly ThreadLocal<Random> random = new ThreadLocal<Random>(() => new Random(Interlocked.Increment(ref seed)));
+        private static int _seed = Environment.TickCount;
 
-        private readonly AsyncManualResetEvent<bool> _haveFirstResults = new AsyncManualResetEvent<bool>();
+        private static readonly ThreadLocal<Random> Random =
+            new ThreadLocal<Random>(() => new Random(Interlocked.Increment(ref _seed)));
 
-        private readonly string _serviceName;
-        private readonly HttpClient _client;
-        private readonly CancellationToken _cancel;
-        private readonly string _lookupUrl;
-        private InformationServiceSet[] _serviceInstances;
-        private WatcherState _state = WatcherState.NotInitialized;
+        private readonly BlockingWatcher<InformationServiceSet[]> _watcher;
 
-        public ServiceWatcher(string serviceName, 
+        public ServiceWatcher(string serviceName,
             HttpClient client, CancellationToken cancel)
         {
-            _serviceName = serviceName;
-            _client = client;
-            _cancel = cancel;
-            _lookupUrl = $"{HttpUtils.ServiceHealthUrl}{serviceName}?passing&index=";
+            string lookupUrl = $"{HttpUtils.ServiceHealthUrl}{serviceName}";
 #pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
-            WatchLoop();
+            _watcher = new BlockingWatcher<InformationServiceSet[]>(lookupUrl, client, cancel);
+            _watcher.WatchLoop();
 #pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
         }
 
         internal async Task<InformationService> GetNextServiceInstanceAsync()
         {
-            if (!await _haveFirstResults.WaitAsync())
-            {
-                return null;
-            }
-            InformationServiceSet[] instances = Volatile.Read(ref _serviceInstances);
-            if (instances.Length > 0)
-            {
-                return instances[random.Value.Next(0, instances.Length)].Service;
-            }
+            var instances = await _watcher.SafeReadAsync();
+            if ((instances != null) && (instances.Length > 0))
+                return instances[Random.Value.Next(0, instances.Length)].Service;
             return null;
-        }
-
-        private async Task WatchLoop()
-        {
-            try
-            {
-                string consulIndex = "0";
-                while (true)
-                {
-                    var result = await _client.GetAsync(_lookupUrl + consulIndex, 
-                        _cancel);
-                    if (!result.IsSuccessStatusCode)
-                    {
-                        if (_state == WatcherState.UsingLiveValues)
-                        {
-                            _state = WatcherState.UsingCachedValues;
-                        }
-                        await Task.Delay(1000);
-                        continue;
-                    }
-                    consulIndex = result.GetConsulIndex();
-                    var content = await result.Content.ReadAsStringAsync();
-                    var listOfServices = JsonConvert.DeserializeObject<InformationServiceSet[]>(content);
-                    Interlocked.Exchange(ref _serviceInstances, listOfServices);
-                    _state = WatcherState.UsingLiveValues;
-                    _haveFirstResults.Set(true);
-                }
-            }
-            catch (TaskCanceledException) { /*nom nom */}
-            catch (ObjectDisposedException) { /*nom nom */}
-        }
-
-        public enum WatcherState
-        {
-            NotInitialized,
-            UsingCachedValues,
-            UsingLiveValues
         }
     }
 }
