cfde92c37b5f0d55cc2a53b2656042b9838f7302
deanshackley <dean.shackley@gmail.com>
2017-01-26 02:04:10 +0000

    Add extension for middleware (#36)

---
+++ samples/Routing/MyMiddleware.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Routing
+{
+    public class MyMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public MyMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            await _next(httpContext);
+        }
+    }
+}
