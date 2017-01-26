8e3485720fc98b11c7a7572c0a57bfa493151645
Shapers <dean.shackley@gmail.com>
2017-01-26 00:42:21 +0000

    Add extension for middleware

---
+++ src/CondenserDotNet.Server/Builder/IConfigurationBuilder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using CondenserDotNet.Server.DataContracts;
+using Microsoft.AspNetCore.Builder.Extensions;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace CondenserDotNet.Server.Builder
+{
+    public interface IConfigurationBuilder
+    {
+        IConfigurationBuilder WithAgentAddress(string agentAdress);
+        IConfigurationBuilder WithLogger(ILoggerProvider logger);
+        IConfigurationBuilder WithAgentPort(int agentPort);
+        IConfigurationBuilder WithHealthRoute(string route);
+        IConfigurationBuilder WithHealthCheck(Func<Task<HealthCheck>> check);
+        IConfigurationBuilder WithHealthCheck(Func<HealthCheck> check);
+        IConfigurationBuilder UsePreRouteMiddleware<T>();
+        IWebHost Build();
+    }
+}
