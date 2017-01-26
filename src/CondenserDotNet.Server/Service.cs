cbc4ca0b42485ffe8bbd14fc46aa8f610848cdb7
Tim Seaward <seawardtim@gmail.com>
2017-01-26 10:42:23 +0000

    Update Service.cs

--- src/CondenserDotNet.Server/Service.cs
+++ src/CondenserDotNet.Server/Service.cs
@@ -130,15 +130,15 @@ namespace CondenserDotNet.Server
                 using (var responseMessage = await _httpClient.SendAsync(requestMessage, HttpCompletionOption.ResponseHeadersRead, context.RequestAborted))
                 {
                     context.Response.StatusCode = (int)responseMessage.StatusCode;
-                    _stats.RecordResponse(context.Response.StatusCode);
+                    _stats?.RecordResponse(context.Response.StatusCode);
                     foreach (var header in responseMessage.Headers)
                     {
-                        context.Response.Headers[header.Key] = header.Value?.ToArray();
+                        context.Response.Headers[header.Key] = header.Value.ToArray();
                     }
 
                     foreach (var header in responseMessage.Content.Headers)
                     {
-                        context.Response.Headers[header.Key] = header.Value?.ToArray();
+                        context.Response.Headers[header.Key] = header.Value.ToArray();
                     }
 
                     // SendAsync removes chunking from the response. This removes the header so it doesn't expect a chunked response.
