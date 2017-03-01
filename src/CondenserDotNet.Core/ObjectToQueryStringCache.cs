bf6c2a556756f63be28b432053ae335ec3baa77b
Drawaes <seawardtim@gmail.com>
2017-03-01 16:10:33 +0000

    Added configuration to release

---
+++ src/CondenserDotNet.Core/ObjectToQueryStringCache.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
+using System.Net.Http;
+
+namespace CondenserDotNet.Core
+{
+    public class ObjectToQueryStringCache
+    {
+        private Dictionary<Type, PropertyInfo[]> _properties = new Dictionary<Type, PropertyInfo[]>();
+
+        public string GetQueryString(string uri, object obj)
+        {
+            var objType = obj.GetType();
+            PropertyInfo[] props;
+            lock(_properties)
+            {
+                if(!_properties.TryGetValue(objType, out props))
+                {
+                    props = objType.GetProperties();
+                    _properties.Add(objType, props);
+                }
+            }
+            var dictionary = new Dictionary<string,string>(props.Length);
+            foreach(var p in props)
+            {
+                var propValue = p.GetValue(obj, null);
+                if (propValue != null)
+                {
+                    dictionary.Add(p.Name, propValue.ToString());
+                }
+            }
+            return Microsoft.AspNetCore.WebUtilities.QueryHelpers.AddQueryString(uri, dictionary);
+        }
+    }
+}
