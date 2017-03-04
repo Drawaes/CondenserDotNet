d22bf91dde2dc7314f8c8792664a5fe1377168bb
Drawaes <seawardtim@gmail.com>
2017-03-04 00:24:25 +0000

    Writing sync?

---
+++ src/CondenserDotNet.Middleware/ChunkingStream.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Server.Kestrel.Internal.Http;
+
+namespace CondenserDotNet.Middleware
+{
+    public class ChunkingStream : Stream
+    {
+        private Stream _innerStream;
+        private static readonly byte[] _endChunkBytes = Encoding.ASCII.GetBytes("\r\n");
+
+        public ChunkingStream()
+        {
+
+        }
+
+        public override bool CanRead => throw new NotImplementedException();
+
+        public override bool CanSeek => throw new NotImplementedException();
+
+        public override bool CanWrite => true;
+
+        public override long Length => throw new NotImplementedException();
+
+        public override long Position { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public Stream InnerStream { get => _innerStream; set => _innerStream = value; }
+
+        public override void Flush()
+        {
+            _innerStream.Flush();
+        }
+
+        public override Task FlushAsync(CancellationToken cancellationToken)
+        {
+            return _innerStream.FlushAsync(cancellationToken);
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SetLength(long value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            var beginChunkBytes = ChunkWriter.BeginChunkBytes(count);
+
+            _innerStream.Write(beginChunkBytes.Array, beginChunkBytes.Offset, beginChunkBytes.Count);
+            _innerStream.Write(buffer, offset, count);
+            _innerStream.Write(_endChunkBytes, 0, _endChunkBytes.Length);
+        }
+
+        public async override Task WriteAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
+        {
+            var beginChunkBytes = ChunkWriter.BeginChunkBytes(count);
+
+            await _innerStream.WriteAsync(beginChunkBytes.Array, beginChunkBytes.Offset, beginChunkBytes.Count, cancellationToken);
+            await _innerStream.WriteAsync(buffer, offset, count, cancellationToken);
+            await _innerStream.WriteAsync(_endChunkBytes, 0, _endChunkBytes.Length, cancellationToken);
+        }
+    }
+}
--- src/CondenserDotNet.Middleware/TrailingHeadersMiddleware.cs
+++ src/CondenserDotNet.Middleware/TrailingHeadersMiddleware.cs
@@ -9,9 +9,6 @@ namespace CondenserDotNet.Middleware
     public class TrailingHeadersMiddleware
     {
         private readonly RequestDelegate _next;
-        private static readonly byte[] s_endOfChunkedBytes = Encoding.ASCII.GetBytes("0\r\n");
-        private static readonly byte[] s_seperator = Encoding.ASCII.GetBytes(": ");
-        private static readonly byte[] s_end = Encoding.ASCII.GetBytes("\r\n");
 
         public TrailingHeadersMiddleware(RequestDelegate next)
         {
@@ -21,35 +18,25 @@ namespace CondenserDotNet.Middleware
         public async Task Invoke(HttpContext context)
         {
             var trailingHeaders = new TrailingHeadersFeature(context);
-            var originalBody = context.Response.Body;
-            //context.Response.Headers["Transfer-Encoding"] = "chunked";
-
+            var stream = new ChunkingStream();
+            stream.InnerStream = context.Response.Body;
+            context.Response.Body = stream;
+            context.Response.Headers["Transfer-Encoding"] = "chunked";
             context.Features.Set<ITrailingHeadersFeature>(trailingHeaders);
             await _next(context);
-            foreach(var feature in context.Features)
+            context.Response.Body = stream.InnerStream;
+            var sb = new StringBuilder();
+            sb.AppendLine("0");
+            foreach (var header in trailingHeaders.Headers)
             {
-                Console.WriteLine(feature.ToString());
+                sb.Append(header.Item1)
+                    .Append(": ")
+                    .Append(header.Item2())
+                    .AppendLine();
             }
-            if(originalBody != context.Response.Body)
-            {
-                throw new NotImplementedException();
-            }
-
-            if (trailingHeaders.Headers.Count > 0)
-            {
-                var sb = new StringBuilder();
-                sb.AppendLine("\r\n0");
-                foreach (var header in trailingHeaders.Headers)
-                {
-                    sb.Append(header.Item1)
-                        .Append(": ")
-                        .Append(header.Item2())
-                        .AppendLine();
-                }
-                sb.AppendLine();
-                var bytes = Encoding.ASCII.GetBytes(sb.ToString());
-                await context.Response.Body.WriteAsync(bytes, 0, bytes.Length);
-            }
+            sb.AppendLine();
+            var bytes = Encoding.ASCII.GetBytes(sb.ToString());
+            await context.Response.Body.WriteAsync(bytes, 0, bytes.Length);
         }
     }
 }
