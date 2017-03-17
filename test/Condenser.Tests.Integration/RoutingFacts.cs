f2fbf8c36ec88d8d6bb2f9574542903ea3a72147
Shapers <dean.shackley@gmail.com>
2017-03-17 00:33:17 +0000

    add routing test

--- test/Condenser.Tests.Integration/RoutingFacts.cs
+++ test/Condenser.Tests.Integration/RoutingFacts.cs
@@ -1,18 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
 using CondenserDotNet.Client;
-using CondenserDotNet.Core;
 using CondenserDotNet.Core.DataContracts;
 using CondenserDotNet.Core.Routing;
 using CondenserDotNet.Server;
-using CondenserDotNet.Server.Builder;
-using CondenserDotNet.Server.DataContracts;
-using CondenserDotNet.Server.Routes;
 using CondenserDotNet.Server.RoutingTrie;
 using Microsoft.AspNetCore.Http;
 using Xunit;
+using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using System.Net;
+using CondenserDotNet.Core;
 
 namespace Condenser.Tests.Integration
 {
@@ -78,5 +78,101 @@ namespace Condenser.Tests.Integration
             var data = new RoutingData(new RadixTree<IService>(createNode));
             return new CustomRouter(null, data);
         }
+
+        [Fact]
+        public async Task CanWeRunRouter()
+        {
+            var hostPort = ServiceManagerConfig.GetNextAvailablePort();
+            var routerPort = ServiceManagerConfig.GetNextAvailablePort();
+
+            var hostService = new WebHostBuilder()
+                .UseKestrel()
+                .UseUrls($"http://*:{hostPort}")
+                .Configure(app =>
+                {
+                    app.Run(async x =>
+                    {
+                        if (x.Request.Path == "/Health")
+                        {
+                            x.Response.StatusCode = 200;
+                            await x.Response.WriteAsync("healthy");
+                        }
+                        else
+                        {
+                            x.Response.StatusCode = 200;
+                            await x.Response.WriteAsync("you called me");
+                        }
+                    });
+                })
+                .Build();
+
+            RoutingHost host = null;
+
+            var routerService = new WebHostBuilder()
+                .UseKestrel()
+                .UseUrls($"http://*:{routerPort}")
+                .ConfigureServices(x =>
+                {
+                    x.AddCondenser();                    
+                })
+                .Configure(app =>
+                {
+                    host = (RoutingHost)app.ApplicationServices
+                        .GetService(typeof(RoutingHost));
+                    app.UseCondenser();
+                })
+                .Build();
+
+            var serviceName = "MyServiceToRoute";
+
+            var options = Options.Create(new ServiceManagerConfig
+            {
+                ServiceName = serviceName,
+                ServicePort = hostPort
+                
+            });
+
+            var serviceManager = new ServiceManager(options);
+
+            await serviceManager
+                .AddHttpHealthCheck("/Health", 5)
+                .AddApiUrl("/myservice")
+                .RegisterServiceAsync();
+
+            try
+            {
+                hostService.Start();
+                routerService.Start();
+
+                var waitForRegister = new AsyncManualResetEvent<bool>();                
+
+                host.OnRouteBuilt = servers =>
+                {
+                    if (servers.ContainsKey(serviceName))
+                    {
+                        waitForRegister.Set(true);
+                    }
+                };
+
+                await waitForRegister.WaitAsync();
+
+                var client = new HttpClient
+                {
+                    Timeout = TimeSpan.FromSeconds(30)
+                };
+
+                var response = await client.GetAsync($"http://localhost:{routerPort}/myservice");
+
+                Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+                Assert.Equal("you called me", await response.Content.ReadAsStringAsync());
+
+            }
+            finally
+            {
+                hostService.Dispose();
+                routerService.Dispose();
+            }
+        }
+
     }
 }
