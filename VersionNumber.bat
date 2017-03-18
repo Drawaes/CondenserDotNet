e527f9c9f27794e798c055d0e9a1bc497d0a3bee
Drawaes <seawardtim@gmail.com>
2017-03-18 11:53:23 +0000

    Update release process

--- VersionNumber.bat
+++ VersionNumber.bat
@@ -2,5 +2,5 @@ IF "%APPVEYOR_REPO_TAG_NAME%"=="" (
 Set suffix=%APPVEYOR_BUILD_NUMBER%
 ) ELSE (
 Set suffix=""
-ECHO ^<Project^>^<Import Project="./releasenotes/%APPVEYOR_REPO_TAG_NAME%.props" /^>^<PropertyGroup^>^<VersionPrefix^>%APPVEYOR_REPO_TAG_NAME%^</VersionPrefix^><^</PropertyGroup^>^</Project^> > version.props
+ECHO ^<Project^>^<Import Project=^"./releasenotes/%APPVEYOR_REPO_TAG_NAME%.props^" /^>^<PropertyGroup^>^<VersionPrefix^>%APPVEYOR_REPO_TAG_NAME%^</VersionPrefix^>^</PropertyGroup^>^</Project^> > version.props
 )
