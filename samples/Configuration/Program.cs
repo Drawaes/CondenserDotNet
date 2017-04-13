f7f332f9b7ced9f4f8031c622184def94a3c3ac7
Shapers <dean.shackley@gmail.com>
2017-04-13 22:24:31 +0100

    update sample

--- samples/Configuration/Program.cs
+++ samples/Configuration/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using CondenserDotNet.Client;
+using CondenserDotNet.Configuration;
+using CondenserDotNet.Configuration.Consul;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 
 namespace Configuration
 {
@@ -10,19 +13,38 @@ namespace Configuration
         public static void Main(string[] args)
         {
             var port = ServiceManagerConfig.GetNextAvailablePort();
-            
+
             //This setup would be done outside of this sample.  
             //The environment variable is passed to the startup to bootstrap
             var environment = "Org1";
             Environment
                 .SetEnvironmentVariable("ASPNETCORE_ENVIRONMENT", environment);
-                       
+
+            var options = Options.Create(new ConsulRegistryConfig
+            {
+                KeyParser = new JsonKeyValueParser()
+            });
+
+            var registry = new ConsulRegistry(options);
+
+            //***Add some config
+            var config = new ConsulConfig
+            {
+                Setting = "Test"
+            };
+
+            registry.SetKeyJsonAsync($"{environment}/ConsulConfig", config).Wait();
+            //***
+
+
+            registry.AddUpdatingPathAsync(environment).Wait();
 
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseUrls($"http://*:{port}")
                 .ConfigureServices(services =>
                 {
+                    services.AddSingleton<IConfigurationRegistry>(registry);
                     services.Configure<ServiceManagerConfig>(opts => opts.ServicePort = port);
                 })
                 .UseStartup<Startup>()
