81d4260146efd818b97e531064f47fd2b0fad2fe
Shapers <dean.shackley@gmail.com>
2017-04-13 22:07:20 +0100

    add previous methods

--- src/CondenserDotNet.Configuration/ServiceCollectionExtensions.cs
+++ src/CondenserDotNet.Configuration/ServiceCollectionExtensions.cs
@@ -1,34 +1,52 @@
-﻿using System;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CondenserDotNet.Configuration
 {
     public static class ServiceCollectionExtensions
     {
+        public static IServiceCollection ConfigureReloadable<TConfig>(this IServiceCollection self,
+            IConfigurationRegistry registry)
+            where TConfig : class
+        {
+            return self.ConfigureReloadable<TConfig>(registry.Root, registry, typeof(TConfig).Name);
+        }
 
-        public static IServiceCollection ConfigureReloadable<TConfig>(this IServiceCollection self, IConfigurationRegistry registry)
-            where TConfig : class =>
-                self.ConfigureReloadable<TConfig>(registry, typeof(TConfig).Name);
+        public static IServiceCollection ConfigureReloadable<TConfig>(this IServiceCollection self,
+            IConfigurationRegistry registry, string sectionName)
+            where TConfig : class
+        {
+            return self.ConfigureReloadable<TConfig>(registry.Root, registry, sectionName);
+        }
 
-        public static IServiceCollection ConfigureReloadable<TConfig>(this IServiceCollection self, IConfigurationRegistry registry, string sectionName)
+
+        public static IServiceCollection ConfigureReloadable<TConfig>(this IServiceCollection self,
+            IConfiguration configuration, IConfigurationRegistry registry)
+            where TConfig : class
+        {
+            return self.ConfigureReloadable<TConfig>(configuration, registry, typeof(TConfig).Name);
+        }
+
+        public static IServiceCollection ConfigureReloadable<TConfig>(this IServiceCollection self,
+            IConfiguration configuration, IConfigurationRegistry registry, string sectionName)
             where TConfig : class
         {
             var initialised = false;
             self.Configure<TConfig>
             (config =>
             {
-                Action bind = () =>
+                void Bind()
                 {
-                    var section = registry.Root.GetSection(sectionName);
+                    var section = configuration.GetSection(sectionName);
                     section.Bind(config);
-                };
+                }
+
                 if (!initialised)
                 {
-                    registry.AddWatchOnEntireConfig(bind);
+                    registry.AddWatchOnEntireConfig(Bind);
                     initialised = true;
                 }
-                bind();
+                Bind();
             });
 
             return self;
