5dbc91630324bf51b3d216c7d3a8fa0592ab9080
Drawaes <seawardtim@gmail.com>
2017-08-02 11:29:09 +0100

    Added deregister service

---
+++ test/Condenser.Tests.Integration/ServiceDeregistrationFacts.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using CondenserDotNet.Client;
+using CondenserDotNet.Client.Services;
+using Microsoft.Extensions.Options;
+using Xunit;
+
+namespace Condenser.Tests.Integration
+{
+    public class ServiceDeregistrationFacts
+    {
+        [Fact]
+        public async Task CheckDeregistrationWorks()
+        {
+            var serviceName = Guid.NewGuid().ToString();
+            var opts = Options.Create(new ServiceManagerConfig() { ServicePort = 2222, ServiceName = serviceName });
+            using (var manager = new ServiceManager(opts))
+            using (var registry = new ServiceRegistry())
+            {
+                var registrationResult = await manager.RegisterServiceAsync();
+                Assert.Equal(true, registrationResult);
+
+                var services = await registry.GetAvailableServicesAsync();
+                Assert.Contains(serviceName, services);
+
+                registrationResult = await manager.DeregisterServiceAsync();
+                Assert.Equal(true, registrationResult);
+
+                services = await registry.GetAvailableServicesAsync();
+                Assert.DoesNotContain(serviceName, services);
+            }
+        }
+    }
+}
