4b6c06223b2f3da8179a643a4e91d774518bf5f3
Tim Seaward <seawardtim@gmail.com>
2018-06-20 23:32:52 +0100

    Working testing still needed

--- src/CondenserDotNet.Core/Consul/EncryptedConsulKeyAclProvider.cs
+++ src/CondenserDotNet.Core/Consul/EncryptedConsulKeyAclProvider.cs
@@ -8,7 +8,7 @@ namespace CondenserDotNet.Core.Consul
 {
     public class EncryptedConsulKeyAclProvider : IConsulAclProvider
     {
-        private string _aclToken;
+        private readonly string _aclToken;
 
         public EncryptedConsulKeyAclProvider(string encryptedKeyName, X509Certificate2 encryptionCertifcate)
         {
@@ -19,7 +19,6 @@ namespace CondenserDotNet.Core.Consul
                 var keyValues = httpClient.GetStringAsync($"/v1/key/{encryptedKeyName}").Result;
                 var keys = JsonConvert.DeserializeObject<KeyValue[]>(keyValues);
                 if (keys.Length != 1) throw new ArgumentException($"Should only be a single key returned from query but had {keys.Length}");
-
                 var decryptedValue = encryptionCertifcate.GetRSAPrivateKey().DecryptValue(Convert.FromBase64String(keys[0].Value));
                 _aclToken = Encoding.UTF8.GetString(decryptedValue);
             }
